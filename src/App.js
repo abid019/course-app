96cc317c01e137bee09c6039f3f0fd73a7a84df5
abid <abid@abids-MacBook-Air.local>
2023-08-30 11:53:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Route,Routes } from "react-router-dom";
+import Navbar from "./Compponents/Navbar";
+// import Home from "./pages/Home"
+import Courses from "./pages/Courses";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import {useState} from 'react'
+import Privatecourse from './pages/Privatecourse'
+import About from "./pages/About";
+import Videos from './Compponents/Videos';
+import Contact from './pages/Contact';
 function App() {
+  const [isLogIn,setIslogin] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-full">
+      <Navbar isLogIn={isLogIn} setIslogin={setIslogin} />
+      <Courses/>
+      <Routes>
+        <Route path="/courses" element={<Courses/>}/>
+        <Route path="/courses" element={
+          <Privatecourse isLogIn = {isLogIn}>
+              <Courses/>
+          </Privatecourse>
+        }/>
+        
+        <Route path="/login" element={<Login setIslogin = {setIslogin}/>}/>
+        <Route path="/singup" element={<Signup setIslogin = {setIslogin}/>}/>
+        <Route path = "/videos" element={<Videos/>} />
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
